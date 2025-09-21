5992053710c4635e2ca7e0bfc782bd427f098e99
Berkay Vuranok <147136880+berkayvuranok@users.noreply.github.com>
2025-09-21 04:14:51 +0300

    Time: 84 ms (15.54%) | Memory: 150.7 MB (37.16%) - LeetSync

---
+++ 1-two-sum/two-sum.dart
@@ -0,0 +1,15 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    List<int> result = [];
+    for(int i=0; i < nums.length-1;i++) {
+        for(int j=i+1;j<nums.length;j++)
+        {
+            if(nums[i]+nums[j] == target)
+            {
+                result.addAll([i,j]);
+            }
+        }
+    }
+    return result;
+  }
+}
