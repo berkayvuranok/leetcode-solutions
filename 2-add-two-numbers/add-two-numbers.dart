0deb67ade35f8e16cc656f14542d5deb4b88340e
Berkay Vuranok <147136880+berkayvuranok@users.noreply.github.com>
2025-09-21 04:27:24 +0300

    Time: 2 ms (62.91%) | Memory: 149.9 MB (69.45%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.dart
@@ -0,0 +1,25 @@
+class Solution {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    if (l1 == null && l2 == null) {
+      return null;
+    }
+
+    int value = (l1?.val ?? 0) + (l2?.val ?? 0);
+    int remainder = value ~/ 10;
+    value %= 10;
+
+    final remainNode = l1?.next ?? l2?.next;
+    final node = new ListNode(value);
+
+    if (remainNode == null) {
+      if (remainder != 0) {
+        node.next = ListNode(remainder);
+      }
+    } else {
+      remainNode.val += remainder;
+      node.next = addTwoNumbers(l1?.next, l2?.next);
+    }
+
+    return node;
+  }
+}
