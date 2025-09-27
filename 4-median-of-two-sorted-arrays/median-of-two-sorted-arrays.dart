1ce2753385e607f46fa64e47f08239b4fb9e5fd9
Berkay Vuranok <147136880+berkayvuranok@users.noreply.github.com>
2025-09-27 12:50:24 +0300

    Time: 2 ms (99.11%) | Memory: 149.4 MB (95.58%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.dart
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.dart
@@ -1,15 +1,28 @@
 class Solution {
   double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
-    final nums = <int>[...nums1, ...nums2]..sort();
-    final middle = nums.length ~/ 2; //Rounded integer middle
+    int totalLength = nums1.length + nums2.length;
+    int medianIndex1 = totalLength ~/ 2;
+    int medianIndex2 = (totalLength % 2 == 0) ? medianIndex1 - 1 : medianIndex1;
 
-    if (middle == nums.length / 2) { //Is rounded middle == real middle
-      int x = middle;
-      int y = middle - 1;
-      return (nums[x] + nums[y]) / 2;
+    int i = 0, j = 0, count = 0;
+    int? current, previous;
+
+    while (count <= medianIndex1) {
+      previous = current;
+
+      if (i < nums1.length && (j >= nums2.length || nums1[i] <= nums2[j])) {
+        current = nums1[i++];
+      } else {
+        current = nums2[j++];
+      }
+
+      count++;
+    }
+
+    if (totalLength % 2 == 0) {
+      return ((previous! + current!) / 2);
     } else {
-      int x = middle;
-      return nums[x].toDouble();
+      return current!.toDouble();
     }
   }
 }
