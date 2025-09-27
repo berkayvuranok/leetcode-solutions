44efa57ea987f483c5c43029f47263f10ae539d4
Berkay Vuranok <147136880+berkayvuranok@users.noreply.github.com>
2025-09-27 15:47:20 +0300

    Time: 6 ms (85.14%) | Memory: 149.1 MB (90.54%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.dart
+++ 12-integer-to-roman/integer-to-roman.dart
@@ -1,33 +1,26 @@
 class Solution {
   String intToRoman(int num) {
-    String convertRoman= "";
-    Map<int,String> roman= {
-        1000:"M",
-        900:"CM",
-        500:"D",
-        400:"CD",
-        100:"C",
-        90:"XC",
-        50:"L",
-        40:"XL",
-        10:"X",
-        9:"IX",
-        5:"V",
-        4:"IV",
-        1:"I",
+    final values = [
+      1000, 900, 500, 400,
+      100, 90, 50, 40,
+      10, 9, 5, 4, 1
+    ];
+    
+    final symbols = [
+      "M", "CM", "D", "CD",
+      "C", "XC", "L", "XL",
+      "X", "IX", "V", "IV", "I"
+    ];
+    
+    String result = "";
 
-    };
-    if(0<=num&& num <=3999) {
-        for(int currentNumber in roman.keys){
-            int remainder=num ~/ currentNumber;
-            if(remainder != 0){
-                convertRoman += roman[currentNumber]! * remainder;
-                num= num- currentNumber * remainder;
-            }
-        }
-        return convertRoman;
-    } else {
-        return "";
+    for (int i = 0; i < values.length; i++) {
+      while (num >= values[i]) {
+        num -= values[i];
+        result += symbols[i];
+      }
     }
+
+    return result;
   }
 }
